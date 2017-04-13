8a072472599dabd485b424f0dd4cdf1849e16c7c
RPI7 <keith.groves@jointheleauge.org>
2017-04-12 21:10:37 -0700

    add new script

---
+++ pullScript.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while [ 1 ]; do
+	if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} |\
+sed 's/\// /g') | cut -f1) ]; then
+        	echo up to date
+	else 
+       		git pull;
+		#remove gradle cache locks
+		find ~/.gradle -type f -name "*.lock" | while read f; do rm $f; done
+		rm -rf .gradle/*/*/*.lock 
+        	./gradlew run
+	fi
+	sleep 5
+done
+
+
