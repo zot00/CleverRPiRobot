d6af8daa69ac62ab12a241da12d737e8e31e5e03
Keith <keith.groves@jointheleague.org>
2017-04-15 17:54:01 -0700

    seperated gradle run from main process

--- pullScript.sh
+++ pullScript.sh
@@ -5,11 +5,12 @@ while [ 1 ]; do
 sed 's/\// /g') | cut -f1) ]; then
         	echo up to date
 	else 
+		echo not up to date... pulling
        		git pull;
 		#remove gradle cache locks
 		find ~/.gradle -type f -name "*.lock" | while read f; do rm $f; done
 		rm -rf .gradle/*/*/*.lock 
-        	./gradlew run
+        	./gradlew run > run.log &
 	fi
 	sleep 5
 done
