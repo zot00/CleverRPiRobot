78868b704b7a0bba71245ab01c8aff5de563d309
zot00 <league@WTS19.attlocal.net>
2017-05-20 17:33:31 -0700

    unstuck able robot

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -12,7 +12,7 @@ import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 
 public class CleverRobot extends IRobotAdapter {
 	Sonar sonar = new Sonar();
-	
+
 	public CleverRobot(IRobotInterface iRobot) {
 		super(iRobot);
 	}
@@ -22,21 +22,34 @@ public class CleverRobot extends IRobotAdapter {
 		IRobotInterface base = new SimpleIRobot();
 		CleverRobot rob = new CleverRobot(base);
 		rob.setup();
-		while(rob.loop()){}
+		while (rob.loop()) {
+		}
 		rob.shutDown();
-		
+
 	}
 
 	private void setup() throws Exception {
-		driveDirect(100,100);
+		driveDirect(100, 100);
 	}
-	
-	private boolean loop() throws Exception{
-		System.out.println("LEFT SONAR: " + sonar.readSonar("left"));
-		Thread.sleep(1000);
-		System.out.println("RIGHT SONAR: " + sonar.readSonar("right"));
-		System.out.println("CENTER SONAR: " + sonar.readSonar("center"));
-		
+
+	private boolean loop() throws Exception {
+		readSensors(100);
+		if (isBumpLeft()) {
+			driveDirect(-500, -500);
+			Thread.sleep(2000);
+			driveDirect(-500, 500);
+			Thread.sleep(1000);
+			driveDirect(500, 500);
+			Thread.sleep(2000);
+		}
+		if (isBumpRight()) {
+			driveDirect(-500, -500);
+			Thread.sleep(2000);
+			driveDirect(500, -500);
+			Thread.sleep(1000);
+			driveDirect(500, 500);
+			Thread.sleep(2000);
+		}
 		return true;
 	}
 
